ea8017ad3dc639d5e6da70cab25e4cbd5b8732ed
Danis Alukaev <d.alukaev@innopolis.university>
2021-05-03 22:07:35 +0300

    ! fixed bug

--- src/main.c
+++ src/main.c
@@ -7,41 +7,28 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
-#define PEERS 1
-
-// 192.168.1.73
+#define PEERS 2
 
 int main() {
+    char request[255], server_address[INET_ADDRSTRLEN], client_address[INET_ADDRSTRLEN];
+    struct Server announcement_server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 1249, PEERS);
     printf("Listening for connections.\n");
-    struct Server announcement_server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 1248, 20);
-    char server_address[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &announcement_server.address.sin_addr, server_address, INET_ADDRSTRLEN);
     printf("IP address of the announcement server: %s \n", server_address);
-    struct LinkedList known_hosts;
+    struct LinkedList known_hosts = linked_list_constructor();
     struct sockaddr_in address;
     int address_length = sizeof(address);
-    while (known_hosts.length <= 1) {
-        int client = accept(announcement_server.socket, (struct sockaddr *) &address, &address_length);
-        char request[255];
+    int connections_left, client;
+
+    // process all requests from peers
+    while (known_hosts.length <= PEERS) {
+        client = accept(announcement_server.socket, (struct sockaddr *) &address, &address_length);
         memset(request, 0, 255);
         read(client, request, 255);
-
-
         struct sockaddr_in *pV4Addr = (struct sockaddr_in *) &address;
         struct in_addr ipAddr = pV4Addr->sin_addr;
-        char client_address[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &ipAddr, client_address, INET_ADDRSTRLEN);
 
-        printf("Peer %s connected.\n", client_address);
-        int connections_left = PEERS - known_hosts.length;
-        char response[255] = "Connected successfully. \n";
-        if (connections_left != 0) {
-            snprintf(response, sizeof response, "%s%s%d \n", response, "Waiting for ", connections_left);
-        } else {
-            snprintf(response, sizeof response, "%s%s \n", response, "All players were connected.");
-        }
-        send(announcement_server.socket, response, strlen(response), 0);
-        close(client);
         short found = 0;
         for (int i = 0; i < known_hosts.length && !found; i++) {
             if (strcmp(client_address, known_hosts.retrieve(&known_hosts, i)) == 0) {
@@ -52,25 +39,37 @@ int main() {
             known_hosts.append(&known_hosts, client_address, sizeof(client_address));
         }
 
-        char serialized_known_hosts[255] = "";
-
-        for (int i = 0; i < known_hosts.length; i++) {
-            char *host = known_hosts.retrieve(&known_hosts, i);
-            strcat(serialized_known_hosts, host);
+        printf("Peer %s connected.\n", client_address);
+        connections_left = PEERS - known_hosts.length;
+        char response[255];
+        if (connections_left != 0) {
+            snprintf(response, sizeof response, "Connected successfully. \nWaiting for %d peer(s). \n",
+                     connections_left);
+        } else {
+            snprintf(response, sizeof response, "Connected successfully. \nAll players were connected.\n");
         }
+        send(client, response, strlen(response), 0);
+        close(client);
+    }
 
-        connections_left = PEERS;
+    // serialize known hosts
+    char serialized_known_hosts[255] = "";
+    for (int i = 0; i < known_hosts.length; i++) {
+        char *host = known_hosts.retrieve(&known_hosts, i);
+        strcat(serialized_known_hosts, host);
+        strcat(serialized_known_hosts, " ");
+    }
 
-        while (connections_left > 0) {
-            client = accept(announcement_server.socket, (struct sockaddr *) &address, &address_length);
-            memset(request, 0, 255);
-            read(client, request, 255);
-
-            printf("Peer %s requested known hosts list.\n", client_address);
-            connections_left--;
-            send(announcement_server.socket, serialized_known_hosts, strlen(serialized_known_hosts), 0);
-            close(client);
-        }
+    // send list of known hosts to all peers
+    connections_left = PEERS;
+    while (connections_left > 0) {
+        client = accept(announcement_server.socket, (struct sockaddr *) &address, &address_length);
+        memset(request, 0, 255);
+        read(client, request, 255);
+        printf("Peer %s requested known hosts list.\n", client_address);
+        connections_left--;
+        send(client, serialized_known_hosts, strlen(serialized_known_hosts), 0);
+        close(client);
     }
 }
 
